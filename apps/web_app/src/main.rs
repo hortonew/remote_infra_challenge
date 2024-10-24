ed8bcc2e82495d790be3527f3b6ff802e0dd4b3f
Erik Horton <hortonew@gmail.com>
2024-10-24 15:33:37 -0600

    Update root path to also work for index.html

--- apps/web_app/src/main.rs
+++ apps/web_app/src/main.rs
@@ -1,9 +1,8 @@
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 use std::env;
 use dotenv::dotenv;
 use serde_json::json;
 
-#[get("/")]
 async fn index() -> impl Responder {
     // Use the INDEX_RESPONSE environment variable if it exists, otherwise use "a"
     let response = env::var("INDEX_RESPONSE").unwrap_or_else(|_| "a".to_string());
@@ -20,7 +19,6 @@ async fn health() -> impl Responder {
     HttpResponse::Ok().json(health_response)
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Load the .env file
@@ -28,10 +26,10 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| "80".to_string());
     let bind_address = format!("0.0.0.0:{}", port);
 
-    // Start the web server on port 80
     HttpServer::new(|| {
         App::new()
-            .service(index)
+            .route("/", web::get().to(index))
+            .route("/index.html", web::get().to(index))
             .service(health)
     })
     .bind(&bind_address)?
